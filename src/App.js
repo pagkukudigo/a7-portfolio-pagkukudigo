f098b2abd216f1a9a557a955a66df12170cbabfb
pagkukudigo <saralyns@uw.edu>
2016-11-18 12:00:27 -0800

    11.18

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 // Application
 import React from 'react';
-import './css/App.css';
+// import '../css/main.css';
 import { Link } from 'react-router';
-import '../node_modules/font-awesome/css/font-awesome.css'
 
 var App = React.createClass({
 	render() {
 		// Return links and show anything inside the <App> component (children)
 		return (
 				<div className="App">
-					<div className = "navbar">
-						<Link className="link" activeClassName='active' to="/"><i className="fa fa-home"></i></Link>
+					<div className="navbar">
+						<Link className="link" activeClassName='active' to="/">Home=</Link>
 						<Link className="link" activeClassName='active' to="/projects">Projects</Link>
                         <Link className="link" activeClassName='active' to="/about">About</Link>
 						<Link className="link" activeClassName='active' to="/contact">Contact</Link>
